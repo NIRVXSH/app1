7e7aeae27809ae3e7110ca4386f77b6a8ff4d791
Sedtanan Mosaleeyanon <setthanan2544@gmail.com>
2023-08-11 14:47:34 +0700

    Class-func-Component
    add Class-component and Func-Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react"
-import './style.css'
+import {Header,Content,Footer} from './func-components'
+import Calendar from "./class-components";
 
 function App() {
     return ( 
-        <>
-        <h2>DeveloperThai.com</h2><br />
-        <div className="title"> React & React Native By Fook </div>
-        </>
-        );
+       <>
+        <Header/>
+        <p><center><Calendar/></center></p>
+        <Content/>
+        <Footer/>
+       </>
+        )
     }
 export default App;
