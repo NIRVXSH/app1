a5141009830c70435759069a56d2933424ebc9c1
Sedtanan Mosaleeyanon <setthanan2544@gmail.com>
2023-08-11 15:06:59 +0700

    add func-props
    add func-props

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import {Header,Content,Footer} from './func-components'
 import Calendar from "./class-components";
 import MsgBox from "./class-props";
+import MsgBox2 from "./func-props";
 
 function App() {
     return ( 
@@ -10,6 +11,13 @@ function App() {
         <MsgBox 
             text="React & React Native"
             color="blue"
+            bgColor="#ccc"
+            fontSize="18pt"
+            border="solid 1px black"
+        />
+        <MsgBox2 
+            text="การใช้ props ใน function component"
+            color="red"
             bgColor="#ccc"
             fontSize="18pt"
             border="solid 1px black"
