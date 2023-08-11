d6de78a34bfdb0871e21cba78665a1477eacb297
Sedtanan Mosaleeyanon <setthanan2544@gmail.com>
2023-08-11 15:01:23 +0700

    class-props
    class-props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react"
 import {Header,Content,Footer} from './func-components'
 import Calendar from "./class-components";
+import MsgBox from "./class-props";
 
 function App() {
     return ( 
        <>
         <Header/>
+        <MsgBox 
+            text="React & React Native"
+            color="blue"
+            bgColor="#ccc"
+            fontSize="18pt"
+            border="solid 1px black"
+        />
         <p><center><Calendar/></center></p>
         <Content/>
         <Footer/>
