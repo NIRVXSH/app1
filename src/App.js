0e8d68a6b598545cab4f9f9cf399a5d3e5aa224c
Sedtanan Mosaleeyanon <setthanan2544@gmail.com>
2023-08-18 13:51:02 +0700

    create banner
    create banner

--- src/App.js
+++ src/App.js
@@ -3,29 +3,10 @@ import {Header,Content,Footer} from './func-components'
 import Calendar from "./class-components";
 import MsgBox from "./class-props";
 import MsgBox2 from "./func-props";
+import Banner from "./banner";
 
-function App() {
-    return ( 
-       <>
-        <Header/>
-        <MsgBox 
-            text="React & React Native"
-            color="blue"
-            bgColor="#ccc"
-            fontSize="18pt"
-            border="solid 1px black"
-        />
-        <MsgBox2 
-            text="การใช้ props ใน function component"
-            color="red"
-            bgColor="#ccc"
-            fontSize="18pt"
-            border="solid 1px black"
-        />
-        <p><center><Calendar/></center></p>
-        <Content/>
-        <Footer/>
-       </>
-        )
-    }
-export default App;
+
+
+export default function App(){
+    return <Banner/>
+};
