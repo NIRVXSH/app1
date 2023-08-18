7c094ecac45001f7e1be98eea454f4f730b409ab
Sedtanan Mosaleeyanon <setthanan2544@gmail.com>
2023-08-18 15:27:28 +0700

    add event data
    add event data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react"
 import Banner from "./banner";
 import { Calculator2 } from "./calculator";
+import { EventData2 } from "./event-data";
 
 export default function App(){
-    return <Calculator2/>
+    return <EventData2/>
 };
