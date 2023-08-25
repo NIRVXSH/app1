7301d3e4f563266677ccadb41cddba4828445f7d
Sedtanan Mosaleeyanon <setthanan2544@gmail.com>
2023-08-25 15:41:48 +0700

    add state-func.js
    add state-func.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { Calculator2 } from "./calculator";
 import { EventData2 } from "./event-data";
 import RefsFunc from "./refs-func";
 import RefsArray from "./refs-array";
+import MessageBox from "./state-func";
 
 export default function App(){
-    return <RefsArray/>
+    return ([<RefsFunc/> ,<RefsArray/>,<MessageBox/>])
 };
