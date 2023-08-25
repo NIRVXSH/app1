9e35464fe2d36ca2f3514038f147e7327ded5a92
Sedtanan Mosaleeyanon <setthanan2544@gmail.com>
2023-08-25 14:35:53 +0700

    Add refs-array
    Add refs-array

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Banner from "./banner";
 import { Calculator2 } from "./calculator";
 import { EventData2 } from "./event-data";
 import RefsFunc from "./refs-func";
+import RefsArray from "./refs-array";
 
 export default function App(){
-    return <RefsFunc/>
+    return <RefsArray/>
 };
