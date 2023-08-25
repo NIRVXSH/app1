e9da9cdad2912fec7473ce026ecded1154f2ab8e
Sedtanan Mosaleeyanon <setthanan2544@gmail.com>
2023-08-25 13:50:47 +0700

    Add refs-func
    Add refs-func

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from "react"
 import Banner from "./banner";
 import { Calculator2 } from "./calculator";
 import { EventData2 } from "./event-data";
+import RefsFunc from "./refs-func";
 
 export default function App(){
-    return <EventData2/>
+    return <RefsFunc/>
 };
