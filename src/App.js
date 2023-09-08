69930e9d92e01a673b55bf3e8e52121a2bce5a4c
Sedtanan Mosaleeyanon <setthanan2544@gmail.com>
2023-09-08 14:18:36 +0700

    Add Context-header.js and context.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ import { EventData2 } from "./event-data";
 import RefsFunc from "./refs-func";
 import RefsArray from "./refs-array";
 import MessageBox from "./state-func";
+import { userContext } from "./context";
+import Header from "./context-header";
 
 export default function App(){
-    return ([<RefsFunc/> ,<RefsArray/>,<MessageBox/>])
+    return (
+        <userContext.Provider value={'Tom Jerry'}>
+            <Header/>
+        </userContext.Provider>
+        )
 };
