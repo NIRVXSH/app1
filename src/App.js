27cf095fc380f9f726004512509556bf62f914a9
Sedtanan Mosaleeyanon <setthanan2544@gmail.com>
2023-09-08 15:30:44 +0700

    Add Context-content2 and header2
    Add Context-content2 and header2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from "react"
-import Banner from "./banner";
-import { Calculator2 } from "./calculator";
-import { EventData2 } from "./event-data";
-import RefsFunc from "./refs-func";
-import RefsArray from "./refs-array";
-import MessageBox from "./state-func";
 import { userContext } from "./context";
-import Header from "./context-header";
+import Header2 from "./context-header2";
+import Content2 from "./context-content2";
 
 export default function App(){
+    let[user,setUser]=React.useState('')
     return (
-        <userContext.Provider value={'Tom Jerry'}>
             <Header/>
+        <userContext.Provider value={[user,setUser]}>
+            <Header2/>
+            <Content2/>
         </userContext.Provider>
         )
 };
