4328db4340d4eedc53d19c19e6f722314210ebe8
Sedtanan Mosaleeyanon <setthanan2544@gmail.com>
2023-09-29 14:24:48 +0700

    bootstrap login

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import Header2 from "./context-header2";
 import Content2 from "./context-content2";
 import Router2 from "./router2";
 import Router3 from "./router3";
+import FormSearch from "./form-search";
+import FormBootstrap from "./form-bootstrap";
+import FormText from "./form-text";
 
 export default function App(){
     
     return (
-        <Router3/>
+        <FormText/>
         )
 };
