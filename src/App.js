ef3d96cc9b4fd5db4add90695f4096609375eb3d
Sedtanan Mosaleeyanon <setthanan2544@gmail.com>
2023-10-06 14:50:15 +0700

    add form validation

--- src/App.js
+++ src/App.js
@@ -8,10 +8,12 @@ import FormSearch from "./form-search";
 import FormBootstrap from "./form-bootstrap";
 import FormText from "./form-text";
 import FormCheck from "./form-check";
+import FormSelectFile from "./form-select-file";
+import FormValidation from "./form-validation";
 
 export default function App(){
     
     return (
-        <FormCheck/>
+        <FormValidation/>
         )
 };
